1b2c34b5239f08625a5a28d1fa9b0f31dd0015f8
vesh95 <vesh95.17@ya.ru>
2020-01-23 23:26:14 +0500

    changed app

--- app.rb
+++ app.rb
@@ -1,13 +1,27 @@
 class App
+  TIME_METHODS = {
+    year: :year,
+    month: :month,
+    day: :day,
+    hour: :hour,
+    minute: :min,
+    second: :sec
+  }
+
   def call(env)
     [
       200,
       { 'Content-Type' => 'text/plain' },
-      ["Stub"]
+      [body(env)]
     ]
   end
 
-  def time
+  def body(env)
+    formats = FormatParams.parse(env["QUERY_STRING"])
+    get_values(formats).join('-')
+  end
 
+  def get_values(formats)
+    formats.map { |format| Time.now.send(TIME_METHODS[format.to_sym]) }
   end
 end
